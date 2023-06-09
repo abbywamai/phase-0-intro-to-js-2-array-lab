b09ef6984156d35c4222bd3883182ed71e98908b
abigail <abigail.wamai@student.moringaschool.com>
2023-06-09 17:23:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const cats = [`Milo`,` Otis`, `Garfield`];
+const name =[`Ralph`]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+cats.shift();
+}
+function destructivelyRemoveFirstCat(){
+    cats.pop();
+}
+const appendcat =[`Broom`]
+function appendCatCat(appendcat){
+    cats.push(appendcat)
+}
+function removeFirstCat(cats) {
+    cats.shift;
+}
